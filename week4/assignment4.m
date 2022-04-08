27c49c6b329e49243e3dce0dedef30ef8f677044
Taten <knight.taten@gmail.com>
2022-04-07 23:35:12 -0600

    Week4 discussion finished

--- week4/assignment4.m
+++ week4/assignment4.m
@@ -14,11 +14,26 @@ dataSets = LoadDataSets();
 %     shg;
 % end
 
-dataSet = dataSets{1};
+dataSet = dataSets{11};
+size(dataSet)
 dist = pdist2(dataSet, dataSet);
+figure()
+heat0 = heatmap(dist);
+heat0.Title = 'Dataset 11 Dist Plot'
+colormap('gray');
+shg;
 [RV,C,I,RI] = VAT(dist);
 RV = RV ./ max(max(RV));
-heatmap(RV);
+figure()
+heat1 = heatmap(RV);
+heat1.Title = 'Dataset 11 VAT'
 colormap('gray');
 shg;
 
+[RiV,RV] = iVAT(dist, true);
+RiV = RiV ./ max(max(RiV));
+figure()
+heat2 = heatmap(RiV);
+heat2.Title = 'Dataset 11 iVAT'
+colormap('gray');
+shg;
